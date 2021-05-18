0071df3a33a9be21ddfa73ce653aaa70caa6afc0
Nicolas Nadé <git@tibuko.com>
2021-05-18 16:05:00 +0200

    Nicolas

--- PTS2Groupe5/src/application/Main.java
+++ PTS2Groupe5/src/application/Main.java
@@ -1,29 +1,20 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {	
-			Parent root = FXMLLoader.load(getClass().getResource("../applicationEnsei/PTUTGRP5Ens.fxml"));
-            Scene scene = new Scene(root);            
-            primaryStage.setScene(scene);
-            primaryStage.setFullScreen(false);
-	        primaryStage.setMaximized(false);
-	        primaryStage.setResizable(false);
-            primaryStage.show();
-            
-	        primaryStage.setTitle("Version enseignante");
-	        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.png")));
-	        
-            
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ PTS2Groupe5/src/applicationEnsei/Main.java
@@ -0,0 +1,35 @@
+package applicationEnsei;
+	
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {	
+			Parent root = FXMLLoader.load(getClass().getResource("PTUTGRP5Ens.fxml"));
+            Scene scene = new Scene(root);            
+            primaryStage.setScene(scene);
+            primaryStage.setFullScreen(false);
+	        primaryStage.setMaximized(false);
+	        primaryStage.setResizable(false);
+            primaryStage.show();
+            
+	        primaryStage.setTitle("Version enseignante");
+	        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
